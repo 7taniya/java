9c10b93c85d32677630e1808055b541c2cfb5191
Taniya  Baral <108205147+7taniya@users.noreply.github.com>
2023-07-25 11:48:32 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,24 @@
+public class sum {
+    public static void main(String args[]){
+        int r=1,s=0;
+        do{
+        s=s+r;
+        r++;
+        }while(r<=10);
+        System.out.println("Sum="+s);
+        int j=0,su=0;
+        while(j<=10){
+               su=su+j;
+               j++;
+        }
+         System.out.println("Sum="+su);
+            int  sum=0;
+        for(int i=1;i<=10;i++){
+       
+          sum=sum+i;
+        }
+        System.out.println("Sum="+sum);
+        
+    }
+    
+}
